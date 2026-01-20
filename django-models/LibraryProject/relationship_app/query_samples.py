68d51e69874336b7431fc67b13ddee3b4377471e
Mercy-Mbaire <mercymbaireh@gmail.com>
2026-01-21 02:06:22 +0300

    Moving query_samples.py to relationship_app directory.

--- django-models/LibraryProject/relationship_app/query_samples.py
+++ django-models/LibraryProject/relationship_app/query_samples.py
@@ -1,5 +1,8 @@
+import sys
 import os
 import django
+
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'LibraryProject.settings')
 django.setup()
