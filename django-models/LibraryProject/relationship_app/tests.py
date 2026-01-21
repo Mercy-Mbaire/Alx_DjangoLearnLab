e591a023cac90a91f66862b2dc58e16dd826013d
Mercy-Mbaire <mercymbaireh@gmail.com>
2026-01-21 23:21:54 +0300

    Views and Access Control.

--- django-models/LibraryProject/relationship_app/tests.py
+++ django-models/LibraryProject/relationship_app/tests.py
@@ -1,3 +1,60 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import UserProfile
+from django.urls import reverse
+from django.test import Client
 
-# Create your tests here.
+class RBACTests(TestCase):
+    def setUp(self):
+        # Create users
+        self.admin_user = User.objects.create_user(username='admin_user', password='password')
+        self.librarian_user = User.objects.create_user(username='librarian_user', password='password')
+        self.member_user = User.objects.create_user(username='member_user', password='password')
+
+
+        # Assign roles
+        from .models import UserProfile
+        
+        # Admin
+        admin_profile = UserProfile.objects.get(user=self.admin_user)
+        admin_profile.role = 'Admin'
+        admin_profile.save()
+        
+        # Librarian
+        librarian_profile = UserProfile.objects.get(user=self.librarian_user)
+        librarian_profile.role = 'Librarian'
+        librarian_profile.save()
+        
+        # Member
+        member_profile = UserProfile.objects.get(user=self.member_user)
+        member_profile.role = 'Member'
+        member_profile.save()
+
+        self.client = Client()
+
+    def test_admin_view_access(self):
+        self.assertTrue(self.client.login(username='admin_user', password='password'), "Login failed for admin_user")
+        response = self.client.get(reverse('admin_view'))
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(username='librarian_user', password='password')
+        response = self.client.get(reverse('admin_view'))
+        self.assertNotEqual(response.status_code, 200) # Should redirect or forbidden
+
+    def test_librarian_view_access(self):
+        self.client.login(username='librarian_user', password='password')
+        response = self.client.get(reverse('librarian_view'))
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(username='member_user', password='password')
+        response = self.client.get(reverse('librarian_view'))
+        self.assertNotEqual(response.status_code, 200)
+
+    def test_member_view_access(self):
+        self.client.login(username='member_user', password='password')
+        response = self.client.get(reverse('member_view'))
+        self.assertEqual(response.status_code, 200)
+
+        self.client.login(username='admin_user', password='password')
+        response = self.client.get(reverse('member_view'))
+        self.assertNotEqual(response.status_code, 200)
--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -26,3 +26,26 @@ class LibraryDetailView(DetailView):
     template_name = 'relationship_app/library_detail.html'
     context_object_name = 'library'
 
+
+from django.contrib.auth.decorators import user_passes_test
+
+def check_admin(user):
+    return user.is_authenticated and user.userprofile.role == 'Admin'
+
+def check_librarian(user):
+    return user.is_authenticated and user.userprofile.role == 'Librarian'
+
+def check_member(user):
+    return user.is_authenticated and user.userprofile.role == 'Member'
+
+@user_passes_test(check_admin)
+def admin_view(request):
+    return render(request, 'relationship_app/admin_view.html')
+
+@user_passes_test(check_librarian)
+def librarian_view(request):
+    return render(request, 'relationship_app/librarian_view.html')
+
+@user_passes_test(check_member)
+def member_view(request):
+    return render(request, 'relationship_app/member_view.html')
